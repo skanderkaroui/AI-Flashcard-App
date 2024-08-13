1b3b6b2c9be599346dc0e1ea0808981c6b19a64f
skanderkaroui <skander.karoui@gmail.com>
2024-08-13 18:02:24 +0100

    trivia home landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,27 @@
-import Image from "next/image";
-import { Dashboard } from "@/components/login";
+import Head from "next/head";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Features from "../components/Features";
+import Pricing from "../components/Pricing";
 
 export default function Home() {
-  return <Dashboard></Dashboard>;
+  return (
+    <div>
+      <Head>
+        <title>TriviaCards - Your Ultimate Flashcard App</title>
+        <meta
+          name="description"
+          content="Master any subject with TriviaCards, the smart flashcard app that revolutionizes your learning experience."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      <main>
+        <Hero />
+        <Features />
+        <Pricing />
+      </main>
+    </div>
+  );
 }
